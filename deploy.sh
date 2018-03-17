029831ead79a95ac9f7f78c7de8ef833c07bb63a
Yusuke Oshiro <yusuke@Yusukes-MacBook-Pro.local>
2018-03-17 18:34:13 +0900

    added ability to update current inventory

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 
 echo "cleaning mdapi/"
 rm -rf mdapi
+echo "making mdapi/"
 mkdir mdapi
+echo "converting to mdapi folder"
 sfdx force:source:convert -r force-app/ -d mdapi/
+echo "deploying..."
 sfdx force:mdapi:deploy -o -u $1 -d mdapi/ -w 10
