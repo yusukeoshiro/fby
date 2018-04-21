6f9f08d77d4294015f2e78eec08e8f4e399e4100
imknism <kenichiro.imamura@salesforce.com>
2018-04-21 18:33:22 +0900

    2018/04/21-0

--- deployNew.sh
+++ deployNew.sh
@@ -1,16 +1,16 @@
-echo "deleting test-by8onqc1dosa@example.com org..."
-sfdx force:org:delete -u test-by8onqc1dosa@example.com -p
+echo "deleting test-ahgiymhoxne5@example.com org..."
+echo "sfdx force:org:delete -u test-ahgiymhoxne5@example.com -p"
 echo "creating a new scratch org..."
-sfdx force:org:create -f config/project-scratch-def.json -a test-by8onqc1dosa@example.com
+sfdx force:org:create -f config/project-scratch-def.json -a test-ahgiymhoxne5@example.com
 echo "pushing code..."
-sfdx force:source:push -u test-by8onqc1dosa@example.com
+sfdx force:source:push -u test-ahgiymhoxne5@example.com
 
 echo "adding permission..."
 sfdx force:user:permset:assign -n inventory_management_system_user
 
 echo "uploading demo data..."
-sfdx force:data:tree:import -p sfdx-out/food_type-FoodTypeLOne__c-FoodTypeLTwo__c-plan.json -u test-by8onqc1dosa@example.com
-sfdx force:data:tree:import -f sfdx-out/Warehouse__c.json -u test-by8onqc1dosa@example.com
+sfdx force:data:tree:import -p sfdx-out/food_type-FoodTypeLOne__c-FoodTypeLTwo__c-plan.json -u test-ahgiymhoxne5@example.com
+sfdx force:data:tree:import -f sfdx-out/Warehouse__c.json -u test-ahgiymhoxne5@example.com
 
 echo "opening the org..."
-sfdx force:org:open -u test-by8onqc1dosa@example.com
+sfdx force:org:open -u test-ahgiymhoxne5@example.com
